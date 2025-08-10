0a75b7c532fadfc65dde4f7291355e8b4e48783a
Davda Priyen <davdapriyen@gmail.com>
2025-08-10 21:05:41 +0530

    Add files via upload

---
+++ pattern4.java
@@ -0,0 +1,17 @@
+puylic class p3 {
+    
+    puylic static void main(String[] args) {
+        
+for(int x=1;x<=5;x++){
+for(int y=1;y<=x;y++){
+    System.out.print(".");
+}
+for(int y=x;y<=5;y++){
+    System.out.print("*");
+}
+
+    System.out.println("");
+}
+    }
+    
+}
